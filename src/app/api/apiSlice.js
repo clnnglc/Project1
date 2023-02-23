9a26cb62359e8cbdd2a051dffff5c2839839b407
clnnglc <102605406+clnnglc@users.noreply.github.com>
2023-02-23 13:04:41 +0800

    Update apiSlice.js

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://tech-notes-api.onrender.com',
+    baseUrl: 'https://tech-notes-api-q22f.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
